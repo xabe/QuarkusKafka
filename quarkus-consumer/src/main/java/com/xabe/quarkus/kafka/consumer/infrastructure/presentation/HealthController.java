668ea0d9c574d5e10b2df99fd9e603db89376d84
chabir atrahouch <chabirae@ext.zara.com>
2020-09-27 21:56:16 +0200

    remove module avro

--- quarkus-consumer/src/main/java/com/xabe/quarkus/kafka/consumer/infrastructure/presentation/HealthController.java
+++ quarkus-consumer/src/main/java/com/xabe/quarkus/kafka/consumer/infrastructure/presentation/HealthController.java
@@ -1,11 +1,13 @@
 package com.xabe.quarkus.kafka.consumer.infrastructure.presentation;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/health")
+@Singleton
 public class HealthController {
 
   @GET
